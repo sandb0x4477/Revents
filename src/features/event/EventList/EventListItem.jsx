e6e90e619a1d227d494589b8f57b121a6a33f1ee
sandb0x4477 <novasettler@gmail.com>
2018-12-19 09:10:41 +1000

    End of section 13

--- src/features/event/EventList/EventListItem.jsx
+++ src/features/event/EventList/EventListItem.jsx
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { Segment, Item, Icon, List, Button } from 'semantic-ui-react';
+import format from 'date-fns/format'
+
 import EventListAtendee from './EventListAtendee';
 
 class EventListItem extends Component {
@@ -23,7 +25,7 @@ class EventListItem extends Component {
         </Segment>
         <Segment>
           <span>
-            <Icon name="clock" /> {event.date} |
+            <Icon name="clock" /> {format(event.date, 'dddd Do MMMM')} at {format(event.date, 'HH:mm')} |
             <Icon name="marker" /> {event.venue}
           </span>
         </Segment>
--- src/index.js
+++ src/index.js
@@ -2,6 +2,9 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
+import ReduxToastr from 'react-redux-toastr';
+import 'react-redux-toastr/lib/css/react-redux-toastr.min.css';
+
 import 'semantic-ui-css/semantic.min.css';
 import './index.css';
 import App from './app/layout/App';
@@ -20,6 +23,7 @@ let render = () => {
     <Provider store={store}>
       <BrowserRouter>
         <ScrollToTop>
+          <ReduxToastr position="bottom-right" transitionIn="fadeIn" transitionOut="fadeOut" />
           <App />
         </ScrollToTop>
       </BrowserRouter>
